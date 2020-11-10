a488c766be6c64259beea0c4929a1966d1b9f3cb
Raazk10 <73226546+Raazk10@users.noreply.github.com>
2020-11-10 23:16:35 +0100

    Multiplication table

--- multiplicationTable.kt
+++ multiplicationTable.kt
@@ -3,7 +3,7 @@ fun main () {
     val num = readLine()!!.toInt()
     //val num = 2
 
-    for (i in 1.. 10) {
+    for (i in 1.. 10) {     // while (i <= 10)  in line 9 i++
       val product = num * i
 
         println("$num * $i = $product")
