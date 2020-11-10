bb02b8e1f0bc90590c06bd784945f0a9f283ac98
Raazk10 <73226546+Raazk10@users.noreply.github.com>
2020-11-10 22:44:17 +0100

    Quadratic equation

--- main.kt
+++ main.kt
@@ -7,23 +7,25 @@ fun main() {
     val root2: Double
     val output:String
 
-    if (determinant > 0) {
-        root1 = (-b + Math.sqrt(determinant)) / (2 * a)
-        root2 = (-b - Math.sqrt(determinant)) / (2 * a)
+    when {
+        determinant > 0 -> {
+            root1 = (-b + Math.sqrt(determinant)) / (2 * a)
+            root2 = (-b - Math.sqrt(determinant)) / (2 * a)
 
-        output = "root1 = %.2f and root2 = %.2f".format(root1, root2)
-    }
-    //Condtition for real and equal roots
-    else if (determinant == 0.0) {
-        root2 = -b / (2 * a)
-        root1 = root2
-        output = "root1 = root2 = %.2f;".format(root1)
+            output = "root1 = %.2f and root2 = %.2f".format(root1, root2)
+        }
+//Condtition for real and equal roots
+        determinant == 0.0 -> {
+            root2 = -b / (2 * a)
+            root1 = root2
+            output = "root1 = root2 = %.2f;".format(root1)
 
-    }
-    else {
-        val realPart = -b / (2 * a)
-        val imaginaryPart = Math.sqrt(-determinant) / (2 * a)
-        output ="root1 = %.2f+%.2fi and root2 = %.2f-%.2fi".format(realPart, imaginaryPart, realPart, imaginaryPart)
+        }
+        else -> {
+            val realPart = -b / (2 * a)
+            val imaginaryPart = Math.sqrt(-determinant) / (2 * a)
+            output ="root1 = %.2f+%.2fi and root2 = %.2f-%.2fi".format(realPart, imaginaryPart, realPart, imaginaryPart)
+        }
     }
 
     println(output)
