a199d9c42b526626db212c9d687ce47172ca5f34
Raazk10 <73226546+Raazk10@users.noreply.github.com>
2020-11-10 18:58:26 +0100

    3rd

---
+++ positiveOrNegative.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val number = -6
+
+    when {
+        number > 0.0 -> println("number is positive")
+        number < 0.0 -> println("number is negative")
+        else -> println(" $number number is 0.")
+    }
+}
